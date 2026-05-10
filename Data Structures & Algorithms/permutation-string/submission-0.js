c7b32c8b690344f41cdfcf7d9616832d1267e913
havya4125 <139970158+havya4125@users.noreply.github.com>
2026-05-10 11:03:57 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    mapsEqual(m1, m2) {
+        if (m1.size !== m2.size) return false;
+        for (let [key, val] of m1) {
+            if (m2.get(key) !== val) return false;
+        }
+        return true;
+    }
+
+    checkInclusion(s1, s2) {
+        const s1Map = new Map();
+        const s2Map = new Map();
+
+        // pusing s1 elements into map
+        for (let i = 0; i < s1.length; i++) {
+            s1Map.set(s1[i], (s1Map.get(s1[i]) || 0) + 1);
+        }
+        const windowSize = s1.length;
+
+        for (let i = 0; i < s2.length - windowSize + 1; i++) {
+            let left = i;
+            let right = i + windowSize - 1;
+            while (left <= right) {
+                s2Map.set(s2[left], (s2Map.get(s2[left]) || 0) + 1);
+                left++;
+            }
+            if (this.mapsEqual(s1Map, s2Map)) return true;
+            s2Map.clear();
+        }
+        return false;
+    }
+}
